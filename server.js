d5af7fc37b7cf227e933c40f03c4efbf5b33b684
Vu Dinh <isthis.vu@gmail.com>
2016-04-11 12:24:00 -0700

    set up server.js to require dependencies and render index.ejs

---
+++ server.js
@@ -0,0 +1,27 @@
+// Require the express module
+var express = require("express");
+var app = express();
+// require body-parser
+var bodyParser = require("body-parser");
+// Integrate body-parser with app
+app.use(bodyParser.urlencoded());
+
+// require path
+var path = require("path");
+
+// set static folder directory
+app.use(express.static(__dirname + "./static"));
+
+// set views folder directory
+app.set("views", path.join(__dirname, "./views"));
+// seting view engine set to ejs
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+	res.render("index")
+})
+
+port = 8000;
+app.listen(port, function(){
+	console.log("**********Listening to port 8000**********")
+})
